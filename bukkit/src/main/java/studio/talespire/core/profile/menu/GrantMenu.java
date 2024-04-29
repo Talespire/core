bb5f3e7bcabd019f673024521f38a7c5300e7964
ogilcher <olivergilcher@gmail.com>
2024-04-29 10:49:56 -0700

    feat: fixed merge issue with GrantMenu

--- bukkit/src/main/java/studio/talespire/core/profile/menu/GrantMenu.java
+++ bukkit/src/main/java/studio/talespire/core/profile/menu/GrantMenu.java
@@ -1,6 +1,5 @@
 package studio.talespire.core.profile.menu;
 
-import lombok.RequiredArgsConstructor;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -14,9 +13,8 @@ import studio.lunarlabs.universe.UniversePlugin;
 import studio.lunarlabs.universe.menus.api.Button;
 import studio.lunarlabs.universe.menus.api.Menu;
 import studio.lunarlabs.universe.util.ItemBuilder;
-import studio.lunarlabs.universe.util.XSound;
 import studio.talespire.core.profile.Profile;
-import studio.talespire.core.profile.menu.button.PlayerHeadButton;
+import studio.talespire.core.profile.menu.button.api.PlayerHeadButton;
 import studio.talespire.core.profile.menu.conversation.PermissionInputPrompt;
 import studio.talespire.core.profile.menu.procedure.RankGrantMenu;
 import studio.talespire.core.profile.menu.procedure.TimeGrantMenu;
