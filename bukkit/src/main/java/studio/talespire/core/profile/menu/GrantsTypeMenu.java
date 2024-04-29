1877eed38eca406168827c4f51ba9ce4623c55ba
ogilcher <olivergilcher@gmail.com>
2024-04-29 14:33:52 -0700

    feat: reformatted GrantsType menu

--- bukkit/src/main/java/studio/talespire/core/profile/menu/GrantsTypeMenu.java
+++ bukkit/src/main/java/studio/talespire/core/profile/menu/GrantsTypeMenu.java
@@ -1,6 +1,5 @@
 package studio.talespire.core.profile.menu;
 
-import lombok.AllArgsConstructor;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -10,36 +9,49 @@ import studio.lunarlabs.universe.menus.api.Button;
 import studio.lunarlabs.universe.menus.api.Menu;
 import studio.lunarlabs.universe.util.ItemBuilder;
 import studio.talespire.core.profile.Profile;
+import studio.talespire.core.profile.menu.button.api.PlayerHeadButton;
 import studio.talespire.core.profile.menu.impl.PermissionGrantsMenu;
 import studio.talespire.core.profile.menu.impl.RankGrantsMenu;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Moose1301
  * @date 4/29/2024
  */
-@AllArgsConstructor
 public class GrantsTypeMenu extends Menu {
     private final Profile profile;
 
+    public GrantsTypeMenu(Profile profile) {
+        this.profile = profile;
+        this.setBordered(true);
+    }
+
     @Override
     public String getTitle(Player player) {
-        return "Pick a grant type for " + profile.getUsername();
+        return "Viewing grants for " + profile.getUsername();
     }
 
     @Override
     public Map<Integer, Button> getButtons(Player player) {
-        return Map.of(
-                3, new PermissionGrantsButton(),
-                5, new RankGrantsButton()
-        );
+
+        Map<Integer, Button> buttons = new HashMap<>();
+
+        buttons.put(getSlot(2, 1), new PermissionGrantsButton());
+        buttons.put(getSlot(4, 1), new PlayerHeadButton(profile.getUsername(), profile.getRank()));
+        buttons.put(getSlot(6, 1), new RankGrantsButton());
+
+        return buttons;
     }
 
     private class PermissionGrantsButton extends Button {
         @Override
         public ItemStack getItem(Player player) {
-            return new ItemBuilder(Material.PAPER).setName(ChatColor.WHITE + "Permission Grants").toItemStack();
+            return new ItemBuilder(Material.PAPER)
+                    .setName(ChatColor.GREEN + "Permission Grants")
+                    .addLoreLine(ChatColor.GRAY + "Click to view permission grants for this player.")
+                    .toItemStack();
         }
 
         @Override
@@ -53,7 +65,10 @@ public class GrantsTypeMenu extends Menu {
     private class RankGrantsButton extends Button {
         @Override
         public ItemStack getItem(Player player) {
-            return new ItemBuilder(Material.PAPER).setName(ChatColor.WHITE + "Rank Grants").toItemStack();
+            return new ItemBuilder(Material.GREEN_WOOL)
+                    .setName(ChatColor.GREEN + "Rank Grants")
+                    .addLoreLine(ChatColor.GRAY + "Click to view rank grants for this player.")
+                    .toItemStack();
         }
 
         @Override
